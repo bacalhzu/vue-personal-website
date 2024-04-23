6c9d203af1398d30fd14008fb038f03f4d9293d3
littlesekii <littlesekii@gmail.com>
2024-04-23 13:09:57 -0300

    project started

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    This website is currently under construction...
+    <p>&copy; littlesekii 2024</p>
+  </main>
+  
+</template>
+
+<style scoped>
+    main {
+        /* background-color: aliceblue; */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 3rem;
+
+        height: 100vh;
+    }
+    p {
+        font-size: 12pt;
+        margin-top: 10px;
+    }
+</style>
